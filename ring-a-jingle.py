121af00d19da685a12c00aad6d070cfabbf2204e
Chris Dilley <codemonkey941@gmail.com>
2022-02-05 16:59:12 -0500

    Connect to AWS S3 and play a song.

--- ring-a-jingle.py
+++ ring-a-jingle.py
@@ -5,12 +5,24 @@ from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
-# https://demo.twilio.com/welcome/voice/
 @app.route('/song', methods=['GET', 'POST'])
 def play_song():
     resp = VoiceResponse()
-    resp.say('Ello, Yello!')
-    return resp
+    try:
+        # Connect to AWS S3 and grab song names
+        s3_client = boto3.client('s3')
+        songs = [k['Key'] for k in s3_client.list_objects_v2(Bucket='ring.a.jingle')['Contents']]
+        url = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': 'ring.a.jingle', 'Key': songs[0]},
+            ExpiresIn=120
+        )
+
+        # Have twilio play the song
+        resp.play(url)
+    except Exception:
+        resp.say("Something went wrong.")
+    return str(resp)
 
 
 if __name__ == "__main__":
