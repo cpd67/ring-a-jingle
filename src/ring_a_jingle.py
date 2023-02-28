739472f3231b90230f592114b31936e8725ae760
Chris Dilley <codemonkey941@gmail.com>
2023-02-27 21:15:42 -0500

    Deploy to fly, remove call to S3 bucket

--- src/ring_a_jingle.py
+++ src/ring_a_jingle.py
@@ -1,6 +1,4 @@
-import boto3
 from flask import Flask
-from random import choice
 from twilio.twiml.voice_response import VoiceResponse
 
 
@@ -12,18 +10,7 @@ def play_song():
     resp = VoiceResponse()
     try:
         # Connect to AWS S3 and grab song names
-        s3_client = boto3.client('s3')
-        songs = [k['Key'] for k in s3_client.list_objects_v2(Bucket='ring.a.jingle')['Contents']]
-
-        # Pick a random song and generate a presigned url for the AWS object associated with that song name
-        url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={'Bucket': 'ring.a.jingle', 'Key': choice(songs)},
-            ExpiresIn=120
-        )
-
-        # Have twilio play the song
-        resp.play(url)
+        resp.say("ring-a-jingle is currently down. It will return at a later date.")
     except Exception:
         resp.say("Unable to obtain a jingle. Please try again later.", voice='alice')
     return str(resp)
