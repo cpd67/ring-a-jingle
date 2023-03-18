a1a549f17ffb3ecd88df0a4151896c4242cae454
Chris Dilley <codemonkey941@gmail.com>
2023-03-17 20:43:06 -0400

    Update more dependencies & move songs

--- src/ring_a_jingle.py
+++ src/ring_a_jingle.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
 
@@ -9,8 +11,7 @@ app = Flask(__name__)
 def play_song():
     resp = VoiceResponse()
     try:
-        # Connect to AWS S3 and grab song names
-        resp.say("ring-a-jingle is currently down. It will return at a later date.")
+
     except Exception:
         resp.say("Unable to obtain a jingle. Please try again later.", voice='alice')
     return str(resp)
