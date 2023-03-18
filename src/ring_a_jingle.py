e5226fca058ad8c024b4d7454fae631cf8ff6899
Chris Dilley <codemonkey941@gmail.com>
2023-03-17 21:43:19 -0400

    Add route to send song file

--- src/ring_a_jingle.py
+++ src/ring_a_jingle.py
@@ -1,6 +1,7 @@
 import os
+from random import choice
 
-from flask import Flask
+from flask import Flask, url_for, request, send_file
 from twilio.twiml.voice_response import VoiceResponse
 
 
@@ -8,13 +9,29 @@ app = Flask(__name__)
 
 
 @app.route('/song', methods=['GET', 'POST'])
-def play_song():
+def get_song_url():
+    """
+    Main entrypoint for Twilio. Sends a response which tells Twilio where to
+    get the song.
+    """
     resp = VoiceResponse()
     try:
-
+        resp.play(url_for("play_song", _external=True))
+        return str(resp)
     except Exception:
-        resp.say("Unable to obtain a jingle. Please try again later.", voice='alice')
-    return str(resp)
+        resp.say('ring-a-jingle is experiencing technical difficulties, please try again later.')
+
+
+@app.route('/play', methods=['GET', 'POST'])
+def play_song():
+    """
+    Send a random song to play.
+    """
+    song_dir = 'songs/'
+    song_list = os.listdir(f'src/{song_dir}')
+    selected_song = choice(song_list)
+
+    return send_file(f'{song_dir}{selected_song}', as_attachment=True, attachment_filename=selected_song, mimetype='audio/mpeg')
 
 
 if __name__ == "__main__":
